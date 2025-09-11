affc4c03765efe998634990e27e4bab271f32579
Yusuf Çelik <ysfclk170@gmail.com>
2025-09-11 14:18:39 +0000

    Freshness test Sütun açıklamaları

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -11,12 +11,11 @@ renamed as (
     select
         orders_id,
         shipping_fee,
-        shipping_fee_1,
         logcost AS log_cost,
-        CAST(ship_cost AS INT64) AS ship_cost
-
+        ship_cost
 
     from source
+   
 
 )
 
